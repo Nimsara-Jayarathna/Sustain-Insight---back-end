3f961e640e2e0f4f55094c1c5b03db0f5432748b
Nimsara Jayarathna <nimsarajayarathna1@gmail.com>
2025-09-18 20:49:23 +0530

    Project Inilization

---
+++ src/test/java/com/news_aggregator/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.news_aggregator.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
