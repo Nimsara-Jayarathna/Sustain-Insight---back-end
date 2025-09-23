7a28c71d8490ce29311f3daadcd1e0fee54f1b4a
IT24103430 <it24103430@my.sliit.lk>
2025-09-23 19:39:44 +0530

    User preferences changing functionality.

---
+++ src/main/java/com/news_aggregator/backend/payload/PreferenceUpdateRequest.java
@@ -0,0 +1,22 @@
+package com.news_aggregator.backend.payload;
+
+import java.util.List;
+
+public class PreferenceUpdateRequest {
+    private String firstName;
+    private String lastName;
+    private List<Long> categoryIds;
+    private List<Long> sourceIds;
+
+    public String getFirstName() { return firstName; }
+    public void setFirstName(String firstName) { this.firstName = firstName; }
+
+    public String getLastName() { return lastName; }
+    public void setLastName(String lastName) { this.lastName = lastName; }
+
+    public List<Long> getCategoryIds() { return categoryIds; }
+    public void setCategoryIds(List<Long> categoryIds) { this.categoryIds = categoryIds; }
+
+    public List<Long> getSourceIds() { return sourceIds; }
+    public void setSourceIds(List<Long> sourceIds) { this.sourceIds = sourceIds; }
+}
