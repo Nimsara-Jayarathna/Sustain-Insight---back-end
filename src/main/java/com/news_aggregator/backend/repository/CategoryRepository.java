024e29c5043b5d52c5ea9e4f1a178c0028abe37c
IT24103430 <it24103430@my.sliit.lk>
2025-09-23 19:38:53 +0530

    fetch all the available categories and sources.

---
+++ src/main/java/com/news_aggregator/backend/repository/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.news_aggregator.backend.repository;
+
+import com.news_aggregator.backend.model.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+}
---
+++ src/main/java/com/news_aggregator/backend/repository/SourceRepository.java
@@ -0,0 +1,9 @@
+package com.news_aggregator.backend.repository;
+
+import com.news_aggregator.backend.model.Source;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SourceRepository extends JpaRepository<Source, Long> {
+}
