fb5d13507ae2346250d3b7ddec9642652bf4dc0a
ruchira lakshan <ruchiralakshan2003@gmail.com>
2025-10-08 00:28:18 +0530

    Create EmailVerificationTokenRepository.java

---
+++ src/main/java/com/news_aggregator/backend/repository/EmailVerificationTokenRepository.java
@@ -0,0 +1,17 @@
+package com.news_aggregator.backend.repository;
+
+import com.news_aggregator.backend.model.EmailVerificationToken;
+import com.news_aggregator.backend.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Repository
+public interface EmailVerificationTokenRepository extends JpaRepository<EmailVerificationToken, Long> {
+    Optional<EmailVerificationToken> findByToken(String token);
+
+    @Transactional
+    void deleteAllByUser(User user);
+}
